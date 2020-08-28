f29af12bb0a5e62f5854daaeda1bf2b68cbca128
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-08-28 14:20:22 +0300

    A test fix

--- src/test/java/com/epam/reportportal/cucumber/ParameterStepReporterTest.java
+++ src/test/java/com/epam/reportportal/cucumber/ParameterStepReporterTest.java
@@ -3,11 +3,9 @@ package com.epam.reportportal.cucumber;
 import com.epam.reportportal.cucumber.integration.TestScenarioReporter;
 import com.epam.reportportal.cucumber.integration.TestStepReporter;
 import com.epam.reportportal.cucumber.integration.util.TestUtils;
-import com.epam.reportportal.listeners.ListenerParameters;
 import com.epam.reportportal.service.ReportPortal;
 import com.epam.reportportal.service.ReportPortalClient;
 import com.epam.reportportal.util.test.CommonUtils;
-import com.epam.ta.reportportal.ws.model.ParameterResource;
 import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
@@ -16,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -57,9 +54,8 @@ public class ParameterStepReporterTest {
 			.collect(Collectors.toList());
 
 	private final ReportPortalClient client = mock(ReportPortalClient.class);
-	private final ListenerParameters parameters = TestUtils.standardParameters();
 	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
-	private final ReportPortal reportPortal = ReportPortal.create(client, parameters, executorService);
+	private final ReportPortal reportPortal = ReportPortal.create(client, TestUtils.standardParameters(), executorService);
 
 	@BeforeEach
 	public void initLaunch() {
@@ -68,29 +64,22 @@ public class ParameterStepReporterTest {
 		TestStepReporter.RP.set(reportPortal);
 	}
 
-	public static final List<Pair<String, Object>> PARAMETERS = Arrays.asList(
-			Pair.of("String", "first"),
-			Pair.of("int", 123),
-			Pair.of("String", "second"),
-			Pair.of("int", 12345),
-			Pair.of("String", "third"),
-			Pair.of("int", 12345678)
-	);
+	@SuppressWarnings("rawtypes")
+	public static final Pair[] PARAMETERS = new Pair[] { Pair.of("String", "first"), Pair.of("int", String.valueOf(123)),
+			Pair.of("String", "second"), Pair.of("int", String.valueOf(12345)), Pair.of("String", "third"),
+			Pair.of("int", String.valueOf(12345678)) };
 
-	public static final List<String> STEP_NAMES = Arrays.asList(
-			String.format("When I have parameter %s", PARAMETERS.get(0).getValue()),
-			String.format("Then I emit number %s on level info", PARAMETERS.get(1).getValue().toString()),
-			String.format("When I have parameter %s", PARAMETERS.get(2).getValue()),
-			String.format("Then I emit number %s on level info", PARAMETERS.get(3).getValue().toString()),
-			String.format("When I have parameter %s", PARAMETERS.get(4).getValue()),
-			String.format("Then I emit number %s on level info", PARAMETERS.get(5).getValue().toString())
-	);
+	public static final String[] STEP_NAMES = new String[] { String.format("When I have parameter %s", PARAMETERS[0].getValue()),
+			String.format("Then I emit number %s on level info", PARAMETERS[1].getValue()),
+			String.format("When I have parameter %s", PARAMETERS[2].getValue()),
+			String.format("Then I emit number %s on level info", PARAMETERS[3].getValue()),
+			String.format("When I have parameter %s", PARAMETERS[4].getValue()),
+			String.format("Then I emit number %s on level info", PARAMETERS[5].getValue()) };
 
 	@Test
 	public void verify_agent_retrieves_parameters_from_request() {
 		TestUtils.runTests(RunOutlineParametersTestStepReporter.class);
 
-		verify(client, times(1)).startTestItem(any());
 		verify(client, times(3)).startTestItem(same(suiteId), any());
 		ArgumentCaptor<StartTestItemRQ> captor = ArgumentCaptor.forClass(StartTestItemRQ.class);
 		verify(client, times(5)).startTestItem(same(testIds.get(0)), captor.capture());
@@ -101,18 +90,19 @@ public class ParameterStepReporterTest {
 		List<StartTestItemRQ> filteredItems = items.stream()
 				.filter(i -> "STEP".equals(i.getType()) && !i.getName().startsWith("Given"))
 				.collect(Collectors.toList());
-		IntStream.range(1, filteredItems.size()).mapToObj(i -> Pair.of(i, filteredItems.get(i))).forEach(e -> {
-			StartTestItemRQ step = e.getValue();
-			assertThat(step, notNullValue());
-			String expectedName = STEP_NAMES.get(e.getKey());
-			assertThat(step.getName(), equalTo(expectedName));
-			assertThat(step.getParameters(), allOf(notNullValue(), hasSize(1)));
-			ParameterResource param = e.getValue().getParameters().get(0);
-			Pair<String, Object> expectedParam = PARAMETERS.get(e.getKey());
+		assertThat(filteredItems, hasSize(6));
+		filteredItems.forEach(i -> assertThat(i.getParameters(), hasSize(1)));
 
-			assertThat(param.getKey(), equalTo(expectedParam.getKey()));
-			assertThat(param.getValue(), equalTo(expectedParam.getValue().toString()));
-		});
+		List<String> itemNames = filteredItems.stream().map(StartTestItemRQ::getName).collect(Collectors.toList());
+		IntStream.range(0, itemNames.size()).filter(i -> i % 2 == 0).forEach(i ->
+				assertThat(itemNames.subList(i, i + 2), containsInAnyOrder(STEP_NAMES[i], STEP_NAMES[i + 1]))
+		);
+
+		List<Pair<String, String>> params = filteredItems.stream()
+				.flatMap(i -> i.getParameters().stream())
+				.map(p -> Pair.of(p.getKey(), p.getValue()))
+				.collect(Collectors.toList());
+		assertThat(params, containsInAnyOrder(PARAMETERS));
 	}
 
 	@Test
